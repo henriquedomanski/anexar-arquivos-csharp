b8237785c4bf785cfd274edfbf8b5094154d9c35
Henrique Domanski <henrydomanski@gmail.com>
2024-03-19 20:13:46 -0300

    Criação do projeto

---
+++ CloudStorage/CloudStorage.API/Controllers/StorageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorage.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StorageController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult UploadImage(IFormFile file)
+    {
+        return Created();
+    }
+}
